6b71382f8d698b05b8b2250908bcf393b6e7ebf4
Shagufta Sajid <119622939+Shagufta-Sajid@users.noreply.github.com>
2023-12-23 02:31:50 +0600

    User Homepage

---
+++ user_homepage_ucode.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+<style>
+
+body {
+  font-family: Helvetica;
+  margin: 0;
+ 
+}
+
+.site-header { 
+  border-bottom: 1px solid #ccc;
+  padding: .5em 1em;
+}
+
+.site-header::after {
+  content: "";
+  display: table;
+  clear: both;
+}
+
+.site-identity {
+  float: left;
+}
+.site-identity img {
+  max-width: 175px;
+  float: left;
+  margin: -40px 10px -45px 0px;
+}
+
+.site-navigation {
+  float: right;
+  font-size: 25px
+  
+}
+
+.site-navigation ul, li {
+  margin: 0; 
+  padding: 0;
+}
+
+.site-navigation li {
+  display: inline-block;
+  margin: 0.2em 2em 0.5em 1em;
+}
+
+input[type=text] {
+  width: 400px;
+  box-sizing: border-box;
+  border: 2px solid #ccc;
+  border-radius: 4px;
+  font-size: 16px;
+  background-color: white;
+  background-image: url("Hnet.com-image.png");
+  background-position: 10px 10px; 
+  background-repeat: no-repeat;
+  transition: width 0.4s ease-in-out;
+  padding: 10px 32px;
+  margin: 12px 0px ;
+}
+
+#navbar {
+  overflow: hidden;
+  background-color: #643B70;
+}
+
+#navbar a {
+  float: left;
+  display: block;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 16px 20px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+#navbar a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+
+
+#navbar a.split {
+  float: right;
+  background-color:#B694BF;
+  color: Whitesmoke;
+}
+#navbar a.split:hover {
+    background-color: red;
+  color: white;
+}
+
+.content {
+  padding: 16px;
+ 
+}
+
+.sticky {
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
+
+.sticky + .content {
+  padding-top: 60px;
+}
+
+
+.footer {
+   position: fixed;
+   left: 0;
+   bottom: 0;
+   width: 100%;
+   background-color: #643B70;
+   color: white;
+   text-align: center;
+}
+
+</style>
+</head>
+<body>
+
+<div class="site-header">
+  <div class="site-identity">
+    <a href="#"><img src="Code for life (2).png" alt="Site Name" /></a>
+    
+  </div>  
+  <nav class="site-navigation">
+    <ul class="nav">
+      <li>
+      <form action="#" method="post">
+        <input type="text"  type="submit" name="id" placeholder="Search By Student ID...    +ENTER">
+      </form>
+      </li>  
+    </ul>
+  </nav>
+</div>
+
+<div id="navbar">
+  <a href="#"><b>Home</b></a>
+  <a href="#"><b>Learn Courses</b></a>
+  <a href="#"><b>My Preparation</b></a>
+  <a href="#"><b>Monthly Contest</b></a>
+  <a href="#"><b>Active Contest</b></a>
+  <a href="#"><b>Notice</b></a>
+  <a href="#"><b>My Profile</b></a>
+  <a href="" class="split"><b>Log Out</b></a>
+</div>
+
+<div class="content">
+    
+<h3>Sticky Navigation Example</h3>
+  <p>The navbar will stick to the top when you reach its scroll position.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  <p>Some text to enable scrolling.. Lorem ipsum dolor sit amet, illum definitiones no quo, maluisset concludaturque et eum, altera fabulas ut quo. Atqui causae gloriatur ius te, id agam omnis evertitur eum. Affert laboramus repudiandae nec et. Inciderint efficiantur his ad. Eum no molestiae voluptatibus.</p>
+  
+</div>
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
+
+<div class="footer">
+        
+        <p><b>UCODE</b> - Powered By <b>Illusion</b></p>
+</div>
+
+
+
+</body>
+</html>
