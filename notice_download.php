580e3f2e54aa1d32580c6ef3423e1da431518200
Shagufta Sajid <119622939+Shagufta-Sajid@users.noreply.github.com>
2023-12-23 02:26:03 +0600

    User Notice

---
+++ notice_download.php
@@ -0,0 +1,23 @@
+ <?php
+if(!empty($_GET['file'])){
+    $fileName  = basename($_GET['file']);
+    $filePath  = "notices/".$fileName;
+    
+    if(!empty($fileName) && file_exists($filePath)){
+        echo "file not exit";
+    }
+    else{
+        
+           //define header
+           header("Cache-Control: public");
+           header("Content-Description: File Transfer");
+           header("Content-Disposition: attachment; filename=$fileName");
+           header("Content-Type: application/zip");
+           header("Content-Transfer-Encoding: binary");
+           
+           //read file 
+           readfile($filePath);
+           exit;
+       }
+    }
+?> 
